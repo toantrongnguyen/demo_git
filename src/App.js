6f300bdf402b3997e58eda686ddfe9c4f842c2bb
ngocnt <tanngocn@gmail.com>
2019-09-25 18:09:38 +0700

    task/456

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 const QUESTIONS = [
-  { id: 1, type: 'text', question: 'Whats your name sss' },
+  { id: 1, type: 'text', question: 'Whats your name?' },
 
 
 
